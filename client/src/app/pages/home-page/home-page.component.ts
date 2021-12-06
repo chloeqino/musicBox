546b0b8776f181aab0ff52feb3c1ea7717957f4c
jmcazare <44219812+jmcazare@users.noreply.github.com>
2021-12-05 16:28:07 -0800

    added playable songs
    songs can be played now

--- client/src/app/pages/home-page/home-page.component.ts
+++ client/src/app/pages/home-page/home-page.component.ts
@@ -7,7 +7,36 @@ import { PredictionEvent } from '../../prediction-event';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-
+  local_songs:any = [
+    {
+      "id": "3CeCwYWvdfXbZLXFhBrbnf",
+      "name": "Love Story (Taylor’s Version)",
+      "artist": "Taylor Swift",
+      "filename" : "Love Story (Taylor’s Version).mp3"
+    }, {
+      "id": "3KkXRkHbMCARz0aVfEt68P",
+      "name": "Sunflower",
+      "artist": "Post Malone",
+      "filename" : "Sunflower.mp3"
+    }, {
+      "id": "2918T2ucPJn7lIxP0IGZnW",
+      "name": "The Violence",
+      "artist": "Rise Against",
+      "filename" : "The Violence.mp3"
+    }, {
+      "id": "6sy3LkhNFjJWlaeSMNwQ62",
+      "name": "Counting Stars",
+      "artist": "One Republic",
+      "filename" : "Counting Stars.mp3"
+    }, {
+      "id": "2918T2ucPJn7lIxP0IGZnW",
+      "name": "Pompeii",
+      "artist": "Bastille",
+      "filename" : "Pompeii.mp3"
+    }
+  ];
+  current:any = null;
+  audio:any = null;
   
   gesture: String = "";
   constructor() { }
@@ -17,6 +46,30 @@ export class HomePageComponent implements OnInit {
 
   prediction(event: PredictionEvent){
     this.gesture = event.getPrediction();
+  }
+
+  nextsong(){
+    if (this.audio != null){
+      this.audio.pause();
+    }
+    var rnum = Math.floor(Math.random() * (this.local_songs.length));
+    this.current = this.local_songs[rnum];
+    this.audio = new Audio("assets/" + this.current["filename"]);
+    this.audio.play();
+  }
+
+  stopsong(){
+    if(this.audio != null) this.audio.pause();
+  }
+
+  resumesong(){
+    if(this.audio != null) this.audio.play();
+  }
+
+  gotopage(){
+    if (this.current != null){
+      //redirect to track page
+    }
   }
   
 }
